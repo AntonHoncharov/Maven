cc74b3340c30fcd1a19bd9524040729be092de65
Honcharov Anton <honcharov.280987@gmail.com>
2022-05-16 17:57:55 +0300

    War and Reace

---
+++ src/test/java/testMavenLessonFour/WarAndPeaceTest.java
@@ -0,0 +1,16 @@
+package testMavenLessonFour;
+
+import mavenLessonFour.WarAndPeace;
+import org.junit.Test;
+
+public class WarAndPeaceTest {
+
+    WarAndPeace warAndPeace = new WarAndPeace();
+
+    @Test
+    public void tesWarAndPeace() throws Exception{
+
+
+
+    }
+}
